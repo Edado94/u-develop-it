8255b259e5103fd2a6c0c663a88c181ee6eee056
Eddie <eddie.echeverria94@gmail.com>
2021-09-08 17:02:33 -0700

    added the routes for majority of all user demands

--- server 2.js
+++ server 2.js
@@ -45,12 +45,7 @@ app.get('/api/candidates', (req, res) => {
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name
-                AS party_name
-                FROM candidates
-                LEFT JOIN parties
-                ON candidates.party_id = parties.id
-                WHERE candidates.id = ?`;
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
